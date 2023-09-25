591be12b863dd0f1c7d3bb857bb431ebec9ac525
Dave <doble.aguila@hotmail.com>
2023-09-25 18:36:52 -0300

    Correcciónes Projects

--- client/src/components/About/Project2/Project2.jsx
+++ client/src/components/About/Project2/Project2.jsx
@@ -1,8 +1,16 @@
+import image from "../../../assets/example.jpg"
+
 const Project2 = () => {
 
     return(
         <div>
             <h1>2</h1>
+            <span>2023</span>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi facilis nobis est explicabo porro incidunt ipsa cumque neque, harum amet quidem consequuntur ratione eaque expedita dignissimos cum laborum iusto totam?</p>
+            <img src={image} alt="" height="280px"/>
+            <span>stack</span>
+            <span>platform</span>
+            <span>website</span>
         </div>
     )
 };
