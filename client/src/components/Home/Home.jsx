59529584a19dfe2e3b669045100944022c24c9a5
Dave <doble.aguila@hotmail.com>
2023-09-26 17:51:55 -0300

    Avances

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,11 +1,11 @@
-import image from "../../assets/example.jpg";
+import image from "../../assets/personal-photo.png";
 import style from "./Home.module.css";
 
 const Home = () => {
 
     return(
         <div className={style.homeContainer}>
-            <h1>Home</h1>
+            <h1>David Varela</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus sequi facilis, praesentium numquam minima, dolorem mollitia labore assumenda voluptas ut enim aliquid quidem odit beatae rerum necessitatibus soluta ab tenetur!</p>
             <img className={style.image} src={image} alt="Fotografía" />
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores repudiandae aliquam quo necessitatibus ea ducimus voluptate, fuga veniam debitis soluta atque dolore quis! Cum rerum cumque autem at. Voluptatem, quo!</p>
