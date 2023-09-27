447629582d21f0cbd49b2235f5c9feb625acd84a
Dave <doble.aguila@hotmail.com>
2023-09-26 21:37:51 -0300

    navBar activeBtn creado

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,21 +1,50 @@
 import style from "./NavBar.module.css";
-import image from "../../assets/computer-logo-icon.svg"
-import { useNavigate } from "react-router-dom";
+import image from "../../assets/computer-logo-icon.svg";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const NavBar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [activeButton, setActiveButton] = useState("");
 
-    const navigate = useNavigate();
+  useEffect(() => {
+    // Obtenemos el nombre de la ruta activa sin el /
+    const activeRoute = location.pathname.slice(1);
+    setActiveButton(activeRoute);
+  }, [location]);
 
-    return (
-        <div className={style.navContainer}>
-            <img className={style.logo} src={image} alt="Logo" />
-            <div className={style.containerBtns}>
-            <button className={style.navBtn} onClick={()=>{navigate("/")}}>Home</button>
-            <button className={style.navBtn} onClick={()=>{navigate("/projects")}}>Projects</button>
-            <button className={style.navBtn} onClick={()=>{navigate("/about")}}>About</button>
-            </div>
-        </div>
-    )
+  return (
+    <div className={style.navContainer}>
+      <img className={style.logo} src={image} alt="Logo" />
+      <div className={style.containerBtns}>
+        <button
+          className={`${style.navBtn} ${activeButton === "" ? style.activeBtn : ""}`}
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          Home
+        </button>
+        <button
+          className={`${style.navBtn} ${activeButton === "projects" ? style.activeBtn : ""}`}
+          onClick={() => {
+            navigate("/projects");
+          }}
+        >
+          Projects
+        </button>
+        <button
+          className={`${style.navBtn} ${activeButton === "about" ? style.activeBtn : ""}`}
+          onClick={() => {
+            navigate("/about");
+          }}
+        >
+          About
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
