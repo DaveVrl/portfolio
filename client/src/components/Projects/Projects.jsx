3a112ba9b726dc31f0838032828aaff3591a44ae
Dave <doble.aguila@hotmail.com>
2023-10-02 19:28:00 -0300

    Avances estilos Desktop

--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -8,20 +8,26 @@ const Projects = () => {
     return (
         <div className={style.containerProjects}>
             <h1 className={style.h1_title}>My Projects</h1>
+            <div className={style.div_a}>
+                <div className={style.div_a_project}>
             <a className={style.a} href="/projects/project1">
                 <img src={lilianaImg} alt="foto" height="260px"/>
                 <h2>Liliana GameStore</h2>
                 <p>Proyecto final de Henry desarrollado en equipo aplicando metodologia SCRUM. Es una tienda virtual enfocada a la venta de productos gamer.</p>
             </a>
             <a className={style.viewMore} href="/projects/project1">Ver más...</a>
+                </div>
             <hr />
+                <div className={style.div_a_project}>
             <a className={style.a} href="/projects/project2">
                 <img src={pokmemonImg} alt="foto"/>
                 <h2>Pokémon SPA</h2>
                 <p>Proyecto individual de Henry. Es una single page application con la temática de Pokémon, desarrollada para demostrar conocimientos adquiridos.</p>
             </a>
             <a className={style.viewMore} href="/projects/project2">Ver más...</a>
+                </div>
             <hr />
+            </div>
             {/* <a className={style.a} href="/projects/project3">
                 <img src={image} alt="foto"/>
                 <h2>Project3</h2>
