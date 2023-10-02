91193f4eebad32b838aee42cc7c7795f5b1c9d16
Dave <doble.aguila@hotmail.com>
2023-10-02 20:49:46 -0300

    info agregada en proyectos

--- client/src/components/Projects/Project2/Project2.jsx
+++ client/src/components/Projects/Project2/Project2.jsx
@@ -17,6 +17,7 @@ const Project2 = () => {
             <img className={style.imgProject} src={image} alt="projectImage" height="280px"/>
             <p className={style.pText}>Pokémon SPA es una single page application desarrollada de forma individual. Este sitio incluye consumo de API, búsquedas, filtros, ordenamiento, formularios controlados, Redux y estados, paginado, rutas, controllers y base de datos.</p>
             <p className={style.pText}>Fue un proyecto individual asignado por Henry para evaluar competencias técnicas. En esta aplicación podrás buscar tus pokémons favoritos, visualizar sus estadísticas y crearte uno a tu gusto.</p>
+            <p className={style.pText}>Tecnologías utilizadas: Frontend (HTML, CSS3, JavaScript con React y Redux), Backend (Node.js con Express.js y JavaScript), Base de Datos (PostgreSQL con Sequelize).</p>
             <a className={style.aBack} href="/projects">Volver</a>
         </div>
     )
