49a0fea0106fc8cd76a14b655c01700b4a418a9a
Dave <doble.aguila@hotmail.com>
2023-10-05 19:37:06 -0300

    (Projects.jsx)Cambio etiqueta a por div cardProject y envuelvo img en div containerIMG

--- client/src/components/Projects/Project2/Project2.jsx
+++ client/src/components/Projects/Project2/Project2.jsx
@@ -14,7 +14,7 @@ const Project2 = () => {
             <p><span>PLATFORM</span> Windows.</p>
             <p><span>WEBSITE</span> <a href="https://spa-pokemon.onrender.com/" target="PokemonSPA">Pokémon SPA</a></p>
             </div>
-            <img className={style.imgProject} src={image} alt="projectImage" height="280px"/>
+            <img className={style.imgProject} src={image} alt="projectImage"/>
             <p className={style.pText}>Pokémon SPA es una single page application desarrollada de forma individual. Este sitio incluye consumo de API, búsquedas, filtros, ordenamiento, formularios controlados, Redux y estados, paginado, rutas, controllers y base de datos.</p>
             <p className={style.pText}>Fue un proyecto individual asignado por Henry para evaluar competencias técnicas. En esta aplicación podrás buscar tus pokémons favoritos, visualizar sus estadísticas y crearte uno a tu gusto.</p>
             <p className={style.pText}>Tecnologías utilizadas: Frontend (HTML, CSS3, JavaScript con React y Redux), Backend (Node.js con Express.js y JavaScript), Base de Datos (PostgreSQL con Sequelize).</p>
--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -10,29 +10,28 @@ const Projects = () => {
             <h1 className={style.h1_title}>My Projects</h1>
             <div className={style.div_a}>
                 <div className={style.div_a_project}>
-            <a className={style.a} href="/projects/project1">
-                <img src={lilianaImg} alt="foto" height="260px"/>
+            <div className={style.cardProject} href="/projects/project1">
+                <div className={style.containerImg}>
+                <img src={lilianaImg} alt="foto"/>
+                </div>
                 <h2>Liliana GameStore</h2>
                 <p>Proyecto final de Henry desarrollado en equipo aplicando metodologia SCRUM. Es una tienda virtual enfocada a la venta de productos gamer.</p>
-            </a>
+            </div>
             <a className={style.viewMore} href="/projects/project1">Ver más</a>
                 </div>
             <hr />
                 <div className={style.div_a_project}>
-            <a className={style.a} href="/projects/project2">
+            <div className={style.cardProject} href="/projects/project2">
+                <div className={style.containerImg}>
                 <img src={pokmemonImg} alt="foto"/>
+                </div>
                 <h2>Pokémon SPA</h2>
                 <p>Proyecto individual de Henry. Es una single page application con la temática de Pokémon, desarrollada para demostrar conocimientos adquiridos.</p>
-            </a>
+            </div>
             <a className={style.viewMore} href="/projects/project2">Ver más</a>
                 </div>
             <hr />
             </div>
-            {/* <a className={style.a} href="/projects/project3">
-                <img src={image} alt="foto"/>
-                <h2>Project3</h2>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga deleniti architecto, veniam natus dolore vitae eligendi assumenda inventore corrupti maiores nostrum voluptates sunt consectetur obcaecati minima quam et molestiae accusantium.</p>
-            </a> */}
         </div>
     )
 };
