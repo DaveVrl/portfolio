34856fe60efbd151298c8b82cfb4f62f3920fbeb
Dave <doble.aguila@hotmail.com>
2023-10-05 19:45:53 -0300

    Project1&2.jsx agrego div containerImg acomodo estilos para aplicar opacidad en img

--- client/src/components/Projects/Project1/Project1.jsx
+++ client/src/components/Projects/Project1/Project1.jsx
@@ -14,7 +14,9 @@ const Project1 = () => {
             <p><span>PLATFORM</span> Windows.</p>
             <p><span>WEBSITE</span> <a href="https://lilianagamesstore.onrender.com/" target="LilianaGamestore">LilianaGamestore</a></p>
             </div>
+            <div className={style.containerImg}>
             <img className={style.imgProject} src={image} alt="projectImage"/>
+            </div>
             <p className={style.pText}>Liliana Gamestore es un e-commerce de tipo B2C (Business to Consumer) desarrollado en equipo bajo metodología SCRUM. Este sitio incluye base de datos, servidor, controllers, rutas, paginados, filtros combinados, ordenamiento, formularios controlados, registro y login de usuarios, crud de admin, pasarela de pagos, favoritos, carrito de compra, reviews de productos, notificaciones, borrado lógico, local storage, Redux y estados.</p>
             <p className={style.pText}> Fue un proyecto grupal asignado por Henry para evaluar competencias técnicas. Mi mayor enfoque fue en el desarrollo de controladores backend y base de datos.</p>
             <p className={style.pText}>Tecnologías utilizadas: Frontend (HTML, CSS3, Bootstrap, JavaScript con React y Redux), Backend (Node.js con Express.js y JavaScript), Base de Datos (PostgreSQL con Sequelize), Payment API (MercadoPago).</p>
