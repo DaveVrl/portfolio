e6e7fba16b97c647595bf9c5b772b7847a679cc8
Dave <doble.aguila@hotmail.com>
2024-02-04 23:39:09 -0300

    Buttons Lenguage en proceso

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -3,6 +3,8 @@ import { initReactI18next } from 'react-i18next';
 import translationES from './dictionary/es.json';
 import translationEN from './dictionary/en.json';
 
+const storedLanguage = localStorage.getItem('language');
+
 i18n
   .use(initReactI18next)
   .init({
@@ -10,7 +12,7 @@ i18n
       en: { translation: translationEN },
       es: { translation: translationES },
     },
-    lng: 'es',
+    lng: storedLanguage || 'es', // Usar el idioma almacenado o predeterminado
     fallbackLng: 'es',
     interpolation: {
       escapeValue: false,
