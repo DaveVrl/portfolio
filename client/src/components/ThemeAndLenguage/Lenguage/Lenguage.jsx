41918abc635c0af09bc7640dddcc6c9d8241e190
Dave <doble.aguila@hotmail.com>
2024-02-05 17:06:07 -0300

    Tipografía en btn language y ajustes de estilo

--- client/src/components/ThemeAndLenguage/Lenguage/Lenguage.jsx
+++ client/src/components/ThemeAndLenguage/Lenguage/Lenguage.jsx
@@ -19,11 +19,13 @@ const Lenguage = () => {
         <div className={style.container}>
           {isSpanish ? (
             <>
+              <span>ES</span>
               <img src={argFlag} className={style.btnLenguage} onClick={() => changeLanguage('en')} />
               <img src={usFlag} className={style.btnLenguage} style={{ display: 'none' }} onClick={() => changeLanguage('en')} />
             </>
           ) : (
             <>
+              <span>EN</span>
               <img src={argFlag} className={style.btnLenguage} style={{ display: 'none' }} onClick={() => changeLanguage('es')} />
               <img src={usFlag} className={style.btnLenguage} onClick={() => changeLanguage('es')} />
             </>
