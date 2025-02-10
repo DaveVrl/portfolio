cb842207773cf181f07d9bb9cdd5c9df336ed9eb
Dave <doble.aguila@hotmail.com>
2025-02-10 20:22:42 -0300

    Trabajando section de Projects..

--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -1,6 +1,7 @@
 import style from "./Projects.module.css";
-import lilianaImg from "../../assets/LilianaGameStore.png";
-import pokmemonImg from "../../assets/PokemonSPA.svg";
+import lilianaImg from "../../assets/LilianaGamestore/cap2.png";
+import pokmemonImg from "../../assets/pokemonspa/cap3.png";
+import taksunity from '../../assets/taskunity/cap1.png';
 // import image from "../../assets/example.jpg"
 import { useTranslation } from "react-i18next";
 import { useNavigate } from 'react-router-dom';
@@ -19,6 +20,21 @@ const Projects = () => {
         <div className={style.containerProjects}>
             <h1 className={style.h1_title}>My Projects</h1>
             <div className={style.div_a}>
+
+            <div className={style.div_a_project}>
+            <div className={style.cardProject}>
+                <div className={style.containerImg}>
+                <img src={taksunity} alt="foto" />
+                </div>
+                <h2>TaskUnity</h2>
+                <p>{t("project_card.description_project_1")}</p>
+            </div>
+            <a className={style.viewMore} onClick={()=>{handleNavigation('/projects/liliana-game-store')}}>{t("buttons.btn_view_more")}</a>
+                </div>
+
+                <hr />
+
+{/*/////////////////////////////////////////////////////////////////////////////////*/}
                 <div className={style.div_a_project}>
             <div className={style.cardProject}>
                 <div className={style.containerImg}>
@@ -29,6 +45,7 @@ const Projects = () => {
             </div>
             <a className={style.viewMore} onClick={()=>{handleNavigation('/projects/liliana-game-store')}}>{t("buttons.btn_view_more")}</a>
                 </div>
+
             <hr />
                 <div className={style.div_a_project}>
             <div className={style.cardProject}>
