d19e85e1e34ca846bfa315bc7acdc5f51273863b
Dave <doble.aguila@hotmail.com>
2025-02-14 19:24:58 -0300

    react tooltip trabajado..

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -7,30 +7,60 @@ import arrow from "../../assets/white-arrow-right.png";
 import emailIcon from "../../assets/white-email-icon.png";
 import downArrow from "../../assets/mailbox.png";
 import { useTranslation } from "react-i18next";
+import { useEffect , useState } from "react";
+import { Tooltip } from 'react-tooltip'
+
 
 const Contact = () => {
   
   const { t } = useTranslation();
   const formRef = useRef(null);
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+  const [ isSubmitLoading , setSubmitLoading ] = useState(false);
 
-    emailjs
-      .sendForm(
-        "service_nd6x6ug", // Service ID
-        "template_1rnewts", // Template ID
-        formRef.current,
-        "Wxeh_SsVpLwknUXu2" // Public Key
-      )
-      .then(
-        () => alert("Mensaje enviado correctamente"),
-        (error) => {
-          console.error("Error al enviar el mensaje", error);
-          alert("Error al enviar el mensaje");
+    useEffect(() => {
+        if (isSubmitLoading) {
+            // Agregar la clase 'cursor-wait' al body
+            document.body.classList.add('cursor-wait');
+        } else {
+            // Eliminar la clase 'cursor-wait' del body
+            document.body.classList.remove('cursor-wait');
         }
-      );
-  };
+    
+        // Limpieza para asegurarnos de que no quede activa la clase
+        return () => document.body.classList.remove('cursor-wait');
+    }, [isSubmitLoading]);
+
+    const [tooltipVisible, setTooltipVisible] = useState(false);
+    const [errorTooltipVisible, setErrorTooltipVisible] = useState(false);
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+      setSubmitLoading(true);
+    
+      emailjs
+        .sendForm(
+          "service_nd6x6ug", // Service ID
+          "template_1rnewts", // Template ID
+          formRef.current,
+          "Wxeh_SsVpLwknUXu2" // Public Key
+        )
+        .then(() => {
+          formRef.current.reset();
+          setTooltipVisible(true);
+          setTimeout(() => {
+            setTooltipVisible(false);
+          }, 4000);
+        })
+        .catch((error) => {
+          console.error("Error al enviar el mensaje", error);
+          setErrorTooltipVisible(true);
+          setTimeout(() => {
+            setErrorTooltipVisible(false);
+          }, 4000);
+        })
+        .finally(() => setSubmitLoading(false));
+    };
 
   return (
     <div className={style.containerContact}>
@@ -55,7 +85,13 @@ const Contact = () => {
         <div className={style.border_form}>
           <div className={style.h3_arrowDown}>
             <h3>{t("contact.title")}</h3>
-            <img src={downArrow} alt="Down arrow icon" />
+            <img 
+            src={downArrow} 
+            alt="Down arrow icon"
+            data-tooltip-id="tooltip-form"
+              />
+              {<Tooltip className={style.tooltip} id="tooltip-form" isOpen={tooltipVisible} place="bottom-end" variant="success" content="¡Tu mensaje ha sido enviado exitosamente!"/>}
+              {<Tooltip id="tooltip-form" isOpen={errorTooltipVisible} place="bottom-end" variant="error" content="¡Ups! No se pudo enviar tu mensaje."/>}
           </div>
           <input type="text" name="user_name" placeholder={t("contact.input1")} required />
           <input type="email" name="user_email" placeholder={t("contact.input2")} required />
