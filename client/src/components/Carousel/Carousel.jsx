8062b6b08c3e79b5ae52df7c59f79f172dbe620a
Dave <doble.aguila@hotmail.com>
2025-02-25 16:41:56 -0300

    Trabajando Carousel...

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -1,14 +1,21 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/zoom';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-
 // import required modules
 import { Zoom, Navigation, Pagination } from 'swiper/modules';
+import img1 from '../../assets/taskunity/cap.png';
+import img2 from '../../assets/taskunity/2-About.png';
+import img3 from '../../assets/taskunity/Login.png';
+import img4 from '../../assets/taskunity/6-Home.png';
+import img5 from '../../assets/taskunity/7-Boards.png';
+import img6 from '../../assets/taskunity/8-Members.png';
+import img7 from '../../assets/taskunity/9-Options.png';
+import img8 from '../../assets/taskunity/10-Work-Board.png';
+import img9 from '../../assets/taskunity/11-Skeletons-Board.png';
 
 const Carousel = () => {
     return (
@@ -28,47 +35,47 @@ const Carousel = () => {
       >
         <SwiperSlide>
           <div className="swiper-zoom-container">
-            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+            <img src={img1} />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="swiper-zoom-container">
-            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+            <img src={img2} />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="swiper-zoom-container">
-            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+            <img src={img3} />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="swiper-zoom-container">
-            <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+            <img src={img4} />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="swiper-zoom-container">
-            <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+            <img src={img5} />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="swiper-zoom-container">
-            <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+            <img src={img6} />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="swiper-zoom-container">
-            <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+            <img src={img7} />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="swiper-zoom-container">
-            <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+            <img src={img8} />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="swiper-zoom-container">
-            <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+            <img src={img9} />
           </div>
         </SwiperSlide>
       </Swiper>
